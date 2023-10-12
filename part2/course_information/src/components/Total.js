d18ce5fb29a225eea4ea641821bf8aac6d011d49
Xiao Li sheng <1522546196@qq.com>
2023-10-12 09:23:47 +0800

    course_information 1 commit

---
+++ part2/course_information/src/components/Total.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+    return (
+        <p>
+            <strong>Number of exercises {total}</strong>
+        </p>
+    );
+}
+
+export default Total;
