9451510c49a0d416fab3b8f689e71c31a6a398d8
Xiao Li sheng <1522546196@qq.com>
2023-10-21 20:49:55 +0800

    part3a2 20:49 2023/10/21

--- part3/courseinfo/index.js
+++ part3/courseinfo/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(express.json());
+app.use(requestLogger);
 
 let notes = [
   {
@@ -74,6 +87,8 @@ app.post("/api/notes", (request, response) => {
   
   response.json(note);
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
