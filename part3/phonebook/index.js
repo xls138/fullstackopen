eb16f04214aa1790520cd4b17a5a94b94f52d028
Xiao Li sheng <1522546196@qq.com>
2023-10-24 08:29:04 +0800

    3.8 8:28 2023/10/24

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 app.use(express.json());
+app.use(morgan("tiny"));
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
   {
@@ -26,11 +31,11 @@ let persons = [
 ];
 
 app.get("/", (request, response) => {
-    response.send("<h1>Hello World!</h1>");
+  response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+  response.json(persons);
 });
 
 app.get("/info", (request, response) => {
@@ -42,37 +47,37 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 app.post("/api/persons", (request, response) => {
-    const body = request.body;
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: "content missing",
-        });
-    }
-    if (persons.find((person) => person.name === body.name)) {
-        return response.status(400).json({
-            error: "name must be unique",
-        });
-    }
-    const person = {
-        id: Math.floor(Math.random() * 10000),
-        name: body.name,
-        number: body.number,
-    };
-    persons = persons.concat(person);
-    response.json(person);
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  const person = {
+    id: Math.floor(Math.random() * 10000),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
