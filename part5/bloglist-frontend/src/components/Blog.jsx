94968809cf3788e5dc79b55e78a2e54471b01b71
Xiao Li sheng <1522546196@qq.com>
2023-11-13 13:59:57 +0800

    5.12

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 const Blog = ({ blog, handleLikes, handleRemoving }) => {
   const [detailsVisible, setDetailsVisible] = useState(false);
 
