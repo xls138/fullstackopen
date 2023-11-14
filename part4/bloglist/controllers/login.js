ca932f89ff490c5f1f5add61df47508ca731ca9b
Xiao Li sheng <1522546196@qq.com>
2023-11-14 15:39:58 +0800

    5.22

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -22,11 +22,8 @@ loginRouter.post('/', async (request, response) => {
         id: user._id,
     }
 
-    const token = jwt.sign(
-        userForToken,
-        process.env.SECRET,
-        { expiresIn: 60 * 60 }
-    )
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
 
     response
         .status(200)
