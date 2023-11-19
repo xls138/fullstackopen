1145366877e1afaf306de9f5bc422dbcad4db883
Xiao Li sheng <1522546196@qq.com>
2023-11-19 19:20:38 +0800

    part6-3

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import noteReducer, { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import noteService from './services/notes'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
 
@@ -15,7 +16,9 @@ const store = configureStore({
   }
 })
 
-console.log(store.getState())
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
