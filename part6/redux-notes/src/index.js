e369a26f4f77f2b9967edc5dd1cd86bdbac0e263
Xiao Li sheng <1522546196@qq.com>
2023-11-19 14:09:00 +0800

    part6-2

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-
 import { Provider } from 'react-redux'
-
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import noteReducer, { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+})
+
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <Provider store={store}>
     <App />
-
-  </Provider>
+  </Provider>,
 )
