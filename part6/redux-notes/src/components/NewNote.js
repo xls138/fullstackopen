c5fbd0b2fb0af60632db28bf68ba1efbe8f4ec94
Xiao Li sheng <1522546196@qq.com>
2023-11-19 20:44:07 +0800

    part6-4

--- part6/redux-notes/src/components/NewNote.js
+++ part6/redux-notes/src/components/NewNote.js
@@ -1,6 +1,5 @@
 import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
-import noteService from '../services/notes'
 
 const NewNote = (props) => {
 
@@ -10,8 +9,7 @@ const NewNote = (props) => {
         event.preventDefault()
         const content = event.target.note.value
         event.target.note.value = ''
-        const newNote = await noteService.createNew(content)
-        dispatch(createNote(newNote))
+        dispatch(createNote(content))
     }
 
     return (
