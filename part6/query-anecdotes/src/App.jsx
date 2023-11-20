3efad971d39ff0ce8a7a4e7f3fec33ecefd5a0ec
Xiao Li sheng <1522546196@qq.com>
2023-11-20 15:41:20 +0800

    6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,56 @@
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
+
+const App = () => {
+  const queryClient = useQueryClient();
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries("anecdotes");
+    },
+  });
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    });
+  };
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+  });
+
+  if (result.isLoading) {
+    return <div>anecdote service not available due to problems in server</div>;
+  }
+
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
